bee56a36c3bce6d4aacae86a1fafe7d96c38d8c2
Mohammad Moslem Uddin <pal.uddin@gmail.com>
2025-12-08 16:54:55 -0500

    Uploaded: Bookbot completed project

--- main.py
+++ main.py
@@ -9,18 +9,25 @@ def get_book_text(path_to_file):
 
 
 def main():
-    book_path = "books/frankenstein.txt"
-    book = (get_book_text(book_path))
-    number_of_words = word_count(book)
-    character_dict = character_count(book)
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {book_path}...")
-    print("----------- Word Count ----------")
-    print(f"Found {number_of_words} total words")
-    print("--------- Character Count -------")
-    sorted_dict = (print_result_dict(character_dict))
-    for item in sorted_dict:
-        print(f'{item["char"]}: {item["num"]}')
-    print("============= END ===============")
+    if (len(sys.argv)) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+
+        # book_path = "books/frankenstein.txt"
+        book_path = sys.argv[1]
+        book = (get_book_text(book_path))
+        number_of_words = word_count(book)
+        character_dict = character_count(book)
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {book_path}...")
+        print("----------- Word Count ----------")
+        print(f"Found {number_of_words} total words")
+        print("--------- Character Count -------")
+        sorted_dict = (print_result_dict(character_dict))
+        for item in sorted_dict:
+            print(f'{item["char"]}: {item["num"]}')
+        print("============= END ===============")
+    
 
 main()
